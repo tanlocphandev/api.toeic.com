bbc5c7dbf7644aeb990bdb3346a56b81587a00fb
LocPhan <ginga550504@gmail.com>
2024-06-17 20:46:02 +0700

    add description

--- src/helpers/asyncHandler.helper.js
+++ src/helpers/asyncHandler.helper.js
@@ -1,5 +1,11 @@
 "use strict";
 
+/**
+ * A higher-order function that wraps asynchronous route handlers to catch any errors that occur during execution.
+ *
+ * @param {Function} fn - The asynchronous route handler function.
+ * @return {Function} - Returns a function that handles asynchronous operations and catches errors.
+ */
 const asyncHandler = (fn) => {
     return (req, res, next) => {
         fn(req, res, next).catch(next);
