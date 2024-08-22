80ec21d3f26d4dc260537ac38409e6562b1ba094
Loc Phan <tanloc.phan.dev@gmail.com>
2024-08-22 09:22:27 +0700

    add vercel.json

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const APP_CONFIGS = require("../configs/app.config");
 const route = express.Router();
 
+route.get("/", (_, res) => {
+    return res.json("Running...");
+});
 route.use(`${APP_CONFIGS.PREFIX}/auth`, require("./auth.route"));
 route.use(`${APP_CONFIGS.PREFIX}/user`, require("./user.route"));
 route.use(`${APP_CONFIGS.PREFIX}/tag`, require("./tag.route"));
